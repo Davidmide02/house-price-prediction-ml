e8e50c2a4c04469ef74b7787e4386552da3ad573
David Adenusi <Davidmide07@gmail.com>
2024-11-23 05:56:31 +0100

    progress

--- app.py
+++ app.py
@@ -21,40 +21,45 @@ def prediction(details):
     
     # X_val = dv.
 
-st.title("House Sale Parameters Input")
+st.title("House Sale prediction model")
+col1, col2 = st.columns(2)
+with col1:
 
 # 1. MSSubClass
-ms_subclass = st.number_input("MSSubClass:the type of dwelling", min_value=1, max_value=200, step=1)
+    ms_subclass = st.number_input("MSSubClass:the type of dwelling", min_value=1, max_value=200, step=1)
 
-# 2. MSZoning
-ms_zoning = st.selectbox("MSZoning:general zoning classification", ["RL", "RM", "C (all)", "FV", "RH"])
+    # 2. MSZoning
+    ms_zoning = st.selectbox("MSZoning:general zoning classification", ["RL", "RM", "C (all)", "FV", "RH"])
 
-# 3. LotArea
-lot_area = st.number_input("LotArea (sq ft): Lot size", min_value=0.0, step=1.0)
+    # 3. LotArea
+    lot_area = st.number_input("LotArea (sq ft): Lot size", min_value=0.0, step=1.0)
 
-# 4. LotConfig
-lot_config = st.selectbox("LotConfig", ["Inside", "Corner", "CulDSac", "FR2", "FR3"])
+    # 4. LotConfig
+    lot_config = st.selectbox("LotConfig", ["Inside", "Corner", "CulDSac", "FR2", "FR3"])
 
-# 5. BldgType
-bldg_type = st.selectbox("BldgType: Type of dwelling", ["1Fam", "2fmCon", "Duplex", "TwnhsE", "Twnhs"])
+    # 5. BldgType
+    bldg_type = st.selectbox("BldgType: Type of dwelling", ["1Fam", "2fmCon", "Duplex", "TwnhsE", "Twnhs"])
+with col2:
+    # 6. OverallCond
+    overall_cond = st.slider("OverallCond:  condition of the house", min_value=1, max_value=10, step=1)
 
-# 6. OverallCond
-overall_cond = st.slider("OverallCond:  condition of the house", min_value=1, max_value=10, step=1)
+    # 7. YearBuilt
+    year_built = st.number_input("YearBuilt", min_value=1800, max_value=2024, step=1)
 
-# 7. YearBuilt
-year_built = st.number_input("YearBuilt", min_value=1800, max_value=2024, step=1)
+    # 8. YearRemodAdd
+    year_remod_add = st.number_input("YearRemodAdd: Remodel date", min_value=1800, max_value=2024, step=1)
 
-# 8. YearRemodAdd
-year_remod_add = st.number_input("YearRemodAdd: Remodel date", min_value=1800, max_value=2024, step=1)
+    # 9. Exterior1st[]
+#     'VinylSd-' 'MetalSd-' 'Wd Sdng-' 'HdBoard-' 'BrkFace-' 'WdShing-' 'CemntBd-'
+#  'Plywood-' 'AsbShng-' 'Stucco-' 'BrkComm-' 'AsphShn-' 'Stone' 'ImStucc'
+#  'CBlock
+    exterior_1st = st.selectbox("Exterior1st: exterior covering", ["AsbShng", "BrkComm", "CemntBd", "HdBoard", "MetalSd", "Plywood", "Stucco", "VinylSd", "Wd Sdng", "WdShing", "BrkFace", "AsphShn", "Stone", "ImStucc", "CBlock"])
 
-# 9. Exterior1st
-exterior_1st = st.selectbox("Exterior1st: exterior covering", ["AsbShng", "BrkComm", "CemntBd", "HdBoard", "MetalSd", "Plywood", "Stucco", "VinylSd", "Wd Sdng", "WdShing"])
+    # 10. BsmtFinSF2
+    bsmt_fin_sf2 = st.number_input("BsmtFinSF2 (sq ft): finished square feet", min_value=0.0, step=1.0)
 
-# 10. BsmtFinSF2
-bsmt_fin_sf2 = st.number_input("BsmtFinSF2 (sq ft): finished square feet", min_value=0.0, step=1.0)
-
-# 11. TotalBsmtSF
-total_bsmt_sf = st.number_input("TotalBsmtSF (sq ft): Total square feet of basement area", min_value=0.0, step=1.0)
+    # 11. TotalBsmtSF
+    total_bsmt_sf = st.number_input("TotalBsmtSF (sq ft): Total square feet of basement area", min_value=0.0, step=1.0)
 
 
 if st.button("Submit"):
